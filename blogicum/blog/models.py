22a58dc5e22e70caf93b68e9796f23a42b86ad8b
SashaMedved <laskov98rus@list.ru>
2026-03-11 18:05:09 +0500

    ss

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -15,7 +15,8 @@ class Category(models.Model):
     slug = models.SlugField(
         'Идентификатор',
         unique=True,
-        help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.'
+        help_text='Идентификатор страницы для URL; '
+                  'разрешены символы латиницы, цифры, дефис и подчёркивание.'
     )
     is_published = models.BooleanField(
         'Опубликовано',
@@ -68,7 +69,8 @@ class Post(models.Model):
     )
     pub_date = models.DateTimeField(
         'Дата и время публикации',
-        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.'
+        help_text='Если установить дату и время в будущем — '
+                  'можно делать отложенные публикации.'
     )
     author = models.ForeignKey(
         User,
